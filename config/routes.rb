b6dba680abd0ac5fa848421e07834d3d6a2b5088
Yuchi Hsueh <bhsueh94@gmail.com>
2022-08-16 13:54:21 -0300

    added create and update routes, controller methods and views

--- config/routes.rb
+++ config/routes.rb
@@ -11,4 +11,11 @@ Rails.application.routes.draw do
   # read
   get "tasks", to: "tasks#index"
   get "tasks/:id", to: "tasks#show", as: :task
+
+  # edit
+  get "tasks/:id/edit", to: "tasks#edit", as: :edit_task
+  patch "tasks/:id", to: "tasks#update"
+
+  #delete
+  delete "tasks/:id", to: "tasks#destroy"
 end
