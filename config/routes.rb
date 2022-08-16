dd35b1d9f0380f8cd96def90eb839686af5d11cc
Yuchi Hsueh <bhsueh94@gmail.com>
2022-08-16 13:27:07 -0300

    added read, index and show controller methods, views and routes

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,12 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "articles#index"
+
+  # create
+  get "tasks/new", to: "tasks#new", as: :new_task
+  post "tasks", to: "tasks#create"
+
+  # read
+  get "tasks", to: "tasks#index"
+  get "tasks/:id", to: "tasks#show", as: :task
 end
